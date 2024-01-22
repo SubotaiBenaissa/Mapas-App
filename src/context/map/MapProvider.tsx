cc21cce48de382a0e691682f09423373108a3921
Alangobe <alangobe01@gmail.com>
2024-01-22 12:02:03 -0500

    Creada función para crear marcador

--- src/context/map/MapProvider.tsx
+++ src/context/map/MapProvider.tsx
@@ -1,16 +1,19 @@
 import { Map, Marker, Popup } from "mapbox-gl";
 import { MapContext } from "./MapContext";
 import { MapReducer } from "./MapReducer";
-import { useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
+import { PlacesContext } from "../";
 
 export interface MapState {
     isMapReady: boolean;
     map?: Map
+    markers: Marker[]
 }
 
 const INITIAL_STATE: MapState = {
     isMapReady: false,
-    map: undefined
+    map: undefined,
+    markers: []
 }
 
 interface Props {
@@ -20,6 +23,27 @@ interface Props {
 export const MapProvider = ({ children }: Props) => {
 
     const [state, dispatch] = useReducer(MapReducer, INITIAL_STATE)
+    const { places } = useContext(PlacesContext)
+
+    useEffect(() => {
+        state.markers.forEach( marker => marker.remove() );
+        const newMarkers: Marker[] = []
+
+        for (const place of places) {
+            const [ lng, lat ] = place.center;
+            const popup = new Popup()
+                .setHTML(`
+                    <h6>${ place.text_es }</h6>
+                    <p>${ place.place_name_es }</p>
+                `)
+            const newMarker = new Marker()
+                .setPopup(popup)
+                .setLngLat([lng, lat])
+                .addTo( state.map! )
+            newMarkers.push(newMarker)
+        }
+    }, [places])
+    
 
     const setMap = (map: Map) => {
 
