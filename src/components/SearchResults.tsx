b1e75170effd1e0804ee0e44fad42bd39068ad85
Alangobe <alangobe01@gmail.com>
2024-01-23 09:06:18 -0500

    Seleccionado lugar y direccionado a posición

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,10 +1,24 @@
-import { useContext } from "react"
-import { PlacesContext } from "../context"
+import { useContext, useState } from "react"
+import { MapContext, PlacesContext } from "../context"
 import { LoadingPlaces } from "./LoadingPlaces"
+import { Feature } from "../interfaces/places"
 
 export const SearchResults = () => {
 
+    const [activeId, setActiveId] = useState('')
     const { places, isLoadingPlaces } = useContext(PlacesContext)
+    const { map } = useContext(MapContext)
+
+    const onPlaceClicked = ( place: Feature ) => {
+
+        const [ lng, lat ] = place.center;
+        setActiveId( place.id )
+
+        map?.flyTo({
+            zoom: 14,
+            center: [ lng, lat ]
+        })
+    }
 
     if( isLoadingPlaces ) {
         return <LoadingPlaces />
@@ -18,17 +32,20 @@ export const SearchResults = () => {
         <ul className="list-group mt-3">
         {
             places.map(place =>(  
-                <li key={ place.id } className="list-group-item list-group-item-action">
+                <li 
+                    key={ place.id } 
+                    className={`list-group-item list-group-item-action pointer ${( activeId === place.id ) ? 'active' : '' } `}
+                    onClick={ () => onPlaceClicked(place) }
+                >
                     <h6>{ place.text_es }</h6>
                     <p 
-                        className="text-muted"
                         style={{
                             fontSize: '12px'
                         }}
                     >
                         {place.place_name}
                     </p>
-                    <button className="btn btn-outline-primary btn-sm">
+                    <button className={`btn ${ activeId === place.id ? 'btn-outline-light' : 'btn-outline-primary' } btn-sm`}>
                         Direcciones
                     </button>
                 </li>
